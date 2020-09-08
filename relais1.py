d4332bf62599257bba7c53ef01717ea45b341678
larfanx230 <victor.lindenbauer@gmx.at>
2020-09-08 09:14:05 +0200

    simple relais test

---
+++ relais1.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO
+import time
+
+RELAIS_1_GPIO = 17
+GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Modus zuweisen
+
+GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # aus
+time.sleep(5)
+GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # an
