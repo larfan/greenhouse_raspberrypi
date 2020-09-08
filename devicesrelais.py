dbe316b9d87b3d6816e6007a2b6940df188e5688
larfanx230 <victor.lindenbauer@gmx.at>
2020-09-08 11:07:10 +0200

    first time relais use

--- devicesrelais.py
+++ devicesrelais.py
@@ -1,9 +1,16 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM) # normal Gpio numbers, not pin numbers
+RELAIS_1_GPIO = 17
+GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Modus zuweisen
+
 
 def relais(widgidx,bool):
     if bool==True:
         print('Gerät ein')
+        if widgidx==0:
+            GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)
     else:
         print('gerät aus')
+        if widgidx==0:
+            GPIO.output(RELAIS_1_GPIO, GPIO.LOW)
