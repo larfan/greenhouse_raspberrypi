5577ffbba12469380c3a29d9c3a30c321c86fc9a
larfanx230 <victor.lindenbauer@gmx.at>
2020-09-08 09:15:11 +0200

    simple relais test

--- relais1.py
+++ relais1.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO
 import time
+GPIO.setmode(GPIO.BCM) # GPIO Nummern statt Board Nummern
 
 RELAIS_1_GPIO = 17
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Modus zuweisen
 
-GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # aus
+GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # aus
 time.sleep(5)
-GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # an
+GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # an
