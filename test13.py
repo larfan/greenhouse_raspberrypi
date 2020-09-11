a601607849a65f089cee0a13eb73c0abd16c1174
larfanx230 <victor.lindenbauer@gmx.at>
2020-09-11 16:55:32 +0200

    more troubleshooting changes

--- test13.py
+++ test13.py
@@ -449,15 +449,15 @@ class guioflabels:
             if element[5] is not None:          #checks if measurand correction should even be affected by time
                 if element[5][0] is not None:
                     if self.memory[index][0]>=element[5][0]:
-                        print('Limit of',element[5][0],'times powering this device per hour, has been exceeded')
-                        file1.write('Limit of '+element[5][0]+' times powering this device per hour, has been exceeded!\n')
+                        print('Limit of',str(element[5][0]),'times powering this device per hour, has been exceeded')
+                        file1.write('Limit of '+(element[5][0])+' times powering this device per hour, has been exceeded!\n')
         
         if argument=='t/24h':
             if element[5] is not None:          #checks if measurand correction should even be affected by time
                 if element[5][1] is not None:
                     if self.memory[index][1]>=element[5][1]:
                         print('The ',element[5][1],'seconds, of allowed daily uptime for this device, has been exceeded')
-                        file1.write('The '+element[5][1]+' seconds, of allowed daily uptime for this device, has been exceeded!\n')
+                        file1.write('The '+str(element[5][1])+' seconds, of allowed daily uptime for this device, has been exceeded!\n')
         #checks if device shouldn't be turned on in certain time frame
         if argument=='timeframe':
             if element[5] is not None:           #checks if measurand correction should be affected by time
