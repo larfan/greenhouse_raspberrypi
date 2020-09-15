e5b409a0faf52052eec9c7e082ef71531d9cd4d1
larfanx230 <victor.lindenbauer@gmx.at>
2020-09-15 12:35:34 +0200

    tweaking values

--- test13.py
+++ test13.py
@@ -7,8 +7,8 @@ import time, random, operator, copy
 
 
 #starting/ideal values for measuring constants
-l3=[20,20,20,20,20]        #soilhumidity, co2, lightintensity, temp, humidity
-l4=[900,20,20,20,20] 
+l3=[995,20,20,20,20]        #soilhumidity, co2, lightintensity, temp, humidity
+l4=[995,20,20,20,20] 
 intervall=[2,2,2,2,2]
 
 #opening file for logging purposes
