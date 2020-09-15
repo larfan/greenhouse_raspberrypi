9316994014337658884e2d381df11f7b72788174
larfanx230 <victor.lindenbauer@gmx.at>
2020-09-15 20:30:32 +0200

    developing feature further

--- test13.py
+++ test13.py
@@ -47,8 +47,10 @@ class measuring:
         '''
         Derweil wird in Checkintervall noch nicht neu gemessen, da ja noch keine Gerät installiert sind
         '''
-        #if measurand==4 or 3:
-        #    self.BMP280()
+         #no correction-simulation needed anymore
+        self.BMP280()
+        self.soilhumidity()
+
         self.check=l3[measurand]-l4[measurand]
         if abs(self.check) >=intervall[index]:
             if self.check < 0:                           #<0 measruand too low
@@ -220,11 +222,11 @@ class guioflabels:
         
         #index of l3,widget,dictionary:{devices,connection,operator}, intervall, [skip correction,skipt simulated-correction], time-dependentlist:[x/h,t/24h,t/am stueck,[von-bis nicht],[if less than n times triggered,ontime duration,possible index of measurand that uses same device,which device to use(high/low) ]]
         #you can inhibit the use of devices, by simply setting the path to None.
-        self.ll=[[0,6,{'high':[1,self.l1[7],'-'],'low':[0,self.l1[3],'+']},2,[None,True],[10,None,10,[0,6],[10,10,None,'low']]],         #soilhumidity
+        self.ll=[[0,6,{'high':[1,self.l1[7],'-'],'low':[0,self.l1[3],'+']},2,[None,None],[10,None,10,[0,6],[10,10,None,'low']]],         #soilhumidity
                     [1,7,{'high':[None,None,'-'],'low':[1,self.l1[4],'+']},2,[None,None],[10,None,10,None,None]],           #co2
-                    [2,8,{'high':[None,None,'-'],'low':[2,self.l1[5],'+']},2,[True,None],[None,5*3600,None,[0,6],None]],   #lightintensity
-                    [3,9,{'high':[1,self.l1[8],'-'],'low':[3,self.l1[6],'+']},2,[None,None],[None,None,None,None,[10,30,1,'high']]],#temperature
-                    [4,10,{'high':[None,None,'-'],'low':[None,None,'+']},2,[None,True],None],                          #humidity
+                    [2,8,{'high':[None,None,'-'],'low':[2,self.l1[5],'+']},2,[True,True],[None,5*3600,None,[0,6],None]],   #lightintensity
+                    [3,9,{'high':[1,self.l1[8],'-'],'low':[3,self.l1[6],'+']},2,[None,True],[None,None,None,None,[10,30,1,'high']]],#temperature
+                    [4,10,{'high':[None,None,'-'],'low':[None,None,'+']},2,[True,True],None],                          #humidity
         
         
         ]
