a0d9023eecdf42700b800a22f9ac34e9b209e6c4
larfanx230 <victor.lindenbauer@gmx.at>
2020-09-16 11:08:33 +0200

    forgot to push relais file

--- devicesrelais.py
+++ devicesrelais.py
@@ -8,13 +8,15 @@ all try statements only are for using program on main deb machine
 '''
 
 RELAIS_GPIO_pump=6
+RELAIS_GPIO_lighbulb=13
 RELAIS_GPIO_fan=19
 RELAIS_GPIO_growlights=26
-relaydevicelist=[RELAIS_GPIO_pump,RELAIS_GPIO_fan,RELAIS_GPIO_growlights]
+relaydevicelist=[RELAIS_GPIO_pump,RELAIS_GPIO_fan,RELAIS_GPIO_growlights,RELAIS_GPIO_lighbulb]
 
 try:
     GPIO.setmode(GPIO.BCM) # normal Gpio numbers, not pin numbers
     GPIO.setup(RELAIS_GPIO_pump, GPIO.OUT) # GPIO Modus zuweisen
+    GPIO.setup(RELAIS_GPIO_lighbulb, GPIO.OUT) # GPIO Modus zuweisen
     GPIO.setup(RELAIS_GPIO_fan, GPIO.OUT) # GPIO Modus zuweisen
     GPIO.setup(RELAIS_GPIO_growlights, GPIO.OUT) # GPIO Modus zuweisen
     for device in relaydevicelist:          #set Gpios to high, as to close relays
@@ -25,7 +27,7 @@ except:
 
 def relais(widgidx,bool):
     try:
-        if widgidx<=2:
+        if widgidx<=3:
             if bool==True:
                 print('Geraet ein')
                 GPIO.output(relaydevicelist[widgidx], GPIO.LOW)
