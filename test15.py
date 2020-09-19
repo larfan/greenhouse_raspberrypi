a76784b0fe48c320ca3d7446e884a4c96bb30c20
larfanx230 <victor.lindenbauer@gmx.at>
2020-09-19 20:14:39 +0200

    very experimental addition to code

--- test15.py
+++ test15.py
@@ -7,8 +7,8 @@ import time, random, operator, copy, traceback
 
 
 #starting/ideal values for measuring constants
-l3=[1010,20,800,24,20]        #soilhumidity, co2, lightintensity, temp, humidity
-l4=[1010,20,800,24,20] 
+l3=[1010,20,350,24,20]        #soilhumidity, co2, lightintensity, temp, humidity
+l4=[1010,20,350,24,20] 
 intervall=[5,2,20,1,2]
 
 #opening file for logging purposes
